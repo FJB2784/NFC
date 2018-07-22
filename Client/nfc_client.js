77607621d57a06bb6535acc2b848be6025945dba
Dolphin_0809 <love_dolphin_0809@outlook.jp>
2018-07-22 20:37:52 +0900

    [追加] クライアントとテストページを追加

---
+++ Client/nfc_client.js
@@ -0,0 +1,45 @@
+function getIDm()
+{
+    const request = new XMLHttpRequest();
+    request.open("GET", "http://localhost:8000/", false);
+
+    request.send();
+
+    if (request.status == 200)
+    {
+        // 読み取り成功
+        return request.responseText;
+    }
+    else
+    {
+        // 読み取り失敗
+        return null;
+    }
+}
+
+// 非同期通信の残骸
+// function getIDmAsync()
+// {
+//     const request = new XMLHttpRequest();
+//     request.open("GET", "http://localhost:8000/");
+
+//     request.addEventListener("load", (event) => {
+//         if (event.target.status == 200)
+//         {
+//             // 読み取り成功
+//             console.log(event.target.responseText);
+//         }
+//         else
+//         {
+//             // 読み取り失敗 500
+//             console.log("カード読み取り失敗");
+//         }
+//     });
+
+//     request.addEventListener("error", (event) => {
+//         // 接続失敗
+//         console.log("サーバに接続できません");
+//     });
+    
+//     request.send();
+// }
